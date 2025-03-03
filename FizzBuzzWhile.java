c9d0fe7217200b0972c45baf9765708135f06a9d
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 11:56:18 +0530

    [Sanya] Add: for multiples of 3 print Fizz instead of the number, for multiples of 5 print Buzz, and for multiples of both print FizzBuzz using while loop

--- FizzBuzzWhile.java
+++ FizzBuzzWhile.java
@@ -20,14 +20,17 @@ public class FizzBuzzWhile {
         while (i <= n) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } 
+	    else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } 
+	    else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else {
+            } 
+	    else {
                 System.out.println(i);
             }
-            i++;
+	    i++;
         }
     }
 }

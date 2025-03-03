347022969f70293f9ced1f86a053ae09f6277d1d
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 13:22:19 +0530

    [Sanya] Add: Check whether the input year is leap year or not

--- LeapYear.java
+++ LeapYear.java
@@ -11,18 +11,21 @@ public class LeapYear {
         // Check if the year is valid (>= 1582)
         if (year < 1582) {
             System.out.println("The year is not valid. Please enter a year >= 1582.");
-        } else {
+        } 
+	else {
             // Part 1: Using multiple if-else statements
             if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 System.out.println(year + " is a Leap Year.");
-            } else {
+            } 
+	    else {
                 System.out.println(year + " is not a Leap Year.");
             }
 
             // Part 2: Using a single if statement with logical operators
             if ((year >= 1582) && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
                 System.out.println(year + " is a Leap Year.");
-            } else if (year >= 1582) {
+            } 
+	    else if (year >= 1582) {
                 System.out.println(year + " is not a Leap Year.");
             }
         }

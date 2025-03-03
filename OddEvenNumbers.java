5865af291ea411b9ddb2523a72c73d209b5c786f
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 12:57:45 +0530

    [Sanya] Add: print odd and even numbers between 1 to the n

--- OddEvenNumbers.java
+++ OddEvenNumbers.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class OddEvenNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Get user input
         System.out.print("Enter a natural number: ");
-        int number = scanner.nextInt();
+        int n = scanner.nextInt();
 
         // Check if the number is a natural number
-        if (number < 1) {
+        if (n < 1) {
             System.out.println(number + " is not a natural number.");
             return;
         }
 
         // Iterate from 1 to the entered number and check for odd or even
-        for (int i = 1; i <= number; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
                 System.out.println(i + " is an even number.");
-            } else {
+            } 
+	    else {
                 System.out.println(i + " is an odd number.");
             }
         }

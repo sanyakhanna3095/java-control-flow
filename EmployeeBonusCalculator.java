c98ac7ffee33942a080113359a63bc39186d5743
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 12:00:03 +0530

    [Sanya] Add: find the bonus of employees based on their years of service

--- EmployeeBonusCalculator.java
+++ EmployeeBonusCalculator.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class EmployeeBonusCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Get employee salary and years of service
         System.out.print("Enter employee salary: ");
-        double salary = scanner.nextDouble();
+        double salary = sc.nextDouble();
 
         System.out.print("Enter years of service: ");
-        int yearsOfService = scanner.nextInt();
+        int yearsOfService = sc.nextInt();
 
         // Check if years of service is more than 5 years and calculate bonus
         if (yearsOfService > 5) {
             double bonus = salary * 0.05;
             System.out.println("Bonus amount: " + bonus);
-        } else {
+        } 
+	else {
             System.out.println("No bonus for less than 5 years of service.");
         }
     }

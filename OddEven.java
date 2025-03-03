4938f9beab4b962f02487348c771c0e55ebd1695
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 11:37:39 +0530

    [Sanya] Add: print if numbers between 1 to n are even or odd

--- OddEven.java
+++ OddEven.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class OddEven {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Get user input
         System.out.print("Enter a natural number: ");
-        int number = scanner.nextInt();
+        int n = sc.nextInt();
 
         // Check if the number is a natural number
-        if (number < 1) {
-            System.out.println(number + " is not a natural number.");
+        if (n < 1) {
+            System.out.println(n + " is not a natural number.");
             return;
         }
 
         // Iterate from 1 to the entered number and check for odd or even
-        for (int i = 1; i <= number; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
                 System.out.println(i + " is an even number.");
             } else {
